819db2144ac0b0a2446244e02b8b7ce50e038b31
cjb <1451055806@qq.com>
2019-04-16 17:40:08 +0800

    3 创建Book,Hero类 4 将apps.py中的应用名添加到settings.py到INSTALLED_APPS中 5 生成迁移文件并执行迁移文件 6 在admin.py注冊创建的类

--- MyTest/BookTest/models.py
+++ MyTest/BookTest/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    btitle = models.CharField(max_length=20)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.btitle
+
+
+class Hero(models.Model):
+    name = models.CharField(max_length=20)
+    gender = models.BooleanField()
+    content = models.CharField(max_length=50)
+    book = models.ForeignKey("Book", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
